1093a8578adb3fe61a936efe51e6c8f2064d4f21
amr <amrkhaledelsisy@gmail.com>
2024-09-19 01:09:54 +0300

    HARDWARE PARTS

---
+++ microcode/motor.h
@@ -0,0 +1,20 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+//Motors pin
+#define MOTOR_RIGHT_PIN1 5
+#define MOTOR_RIGHT_PIN2 8
+#define MOTOR_LEFT_PIN1 4
+#define MOTOR_LEFT_PIN2 0
+
+
+
+moveForward(int distance);
+
+void turnLeft();
+
+void stop();
+
+void turnRight();
+
+#endif
